ebd68d2b89bff173c2f3f634e1b52714f6aa2f57
J <118442144+model-map@users.noreply.github.com>
2023-02-23 17:24:16 +0530

    Add Empress Police post

--- js/script.js
+++ js/script.js
@@ -93,6 +93,7 @@ const bonusTitle=[
     'ANTI-WOKE RESPONSES REVEAL 02-21',
 'THE TOP TIER CONTRADICTION & HYPOCRISY OF THE HUMAN RACE (removed by Reddit)',
 'Important Message To : r/PiratedGames Moderators & Fitgirl',
+'Finally caught by the police, goodjob everyone.',
 '[Digital Piracy] The rise of EMPRESS - How one woman turned the pirate underworld on its head, waged a solo war against the entire game industry (and won), went mad with power, started a messianic cult based on high school-level philosophy, and faked her own arrest to spite her rivals and haters'
 ];
 
@@ -101,6 +102,7 @@ const bonusLink=[
     'https://web.archive.org/web/20230222032715/https://telegra.ph/ANTI-WOKE-RESPONSES-REVEAL-02-21',
     'https://web.archive.org/web/20230221094226/https://old.reddit.com/r/EmpressEvolution/comments/117ywff/the_top_tier_contradiction_hypocrisy_of_the_human/',
     'https://web.archive.org/web/20230220225629/https://old.reddit.com/r/EmpressEvolution/comments/117e8tz/important_message_to_rpiratedgames_moderators/',
+    'https://web.archive.org/web/20210227191727/https://old.reddit.com/r/CrackWatch/comments/lqs91b/finally_caught_by_the_police_goodjob_everyone/',
     'https://web.archive.org/web/20211226221641/https://old.reddit.com/r/HobbyDrama/comments/rowk83/digital_piracy_the_rise_of_empress_how_one_woman/'
 ]
 
